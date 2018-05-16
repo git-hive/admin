c7a4c1dc6807b611b8c888cd6cdd7a58ca0f26e9
João Fernandes <joaovitorfernandes2@gmail.com>
2018-05-16 16:26:35 -0300

    Add vuetify baseline layout

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-navigation-drawer
+    fixed
+    v-model="isVisible"
+    app
+  >
+    <v-list dense>
+      <v-list-tile @click.stop="$router.push('/')">
+        <v-list-tile-action>
+          <v-icon>home</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Home</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile @click.stop="$router.push('/about')">
+        <v-list-tile-action>
+          <v-icon>contact_mail</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>About</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "NavigationDrawer",
+  props: ["isVisible"]
+};
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-toolbar color="indigo" dark fixed app>
+    <v-toolbar-side-icon @click.stop="$emit('drawer-toggle')"></v-toolbar-side-icon>
+    <v-toolbar-title>Hive</v-toolbar-title>
+  </v-toolbar>
+</template>
