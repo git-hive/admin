a810e4fec229255ce2870cd5d0e321349b0a921f
João Fernandes <joaovitorfernandes2@gmail.com>
2018-05-17 10:33:34 -0300

    Add firebase auth methods and listener

--- src/main.js
+++ src/main.js
@@ -1,18 +1,31 @@
-import Vue from "vue";
-import Vuetify from "vuetify";
-import App from "./App.vue";
-import router from "./router";
-import store from "@/store/store";
 import "./firebase/main";
 import "./registerServiceWorker";
 import "vuetify/dist/vuetify.min.css";
+
+import Vue from "vue";
+import Vuetify from "vuetify";
+import { auth } from "firebase";
+
+import App from "./App.vue";
+import router from "./router";
+import store from "@/store/store";
 
 Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
-new Vue({
+const app = new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
+
+auth().onAuthStateChanged(user => {
+  const { dispatch } = app.$store;
+
+  if (user) {
+    dispatch("setUser", user);
+  } else {
+    dispatch("unsetUser");
+  }
+});
