37ee049a2f2e1e3b505bf8275f9b35a40f3752b5
João Fernandes <joaovitorfernandes2@gmail.com>
2018-05-17 10:35:01 -0300

    Add sign in/out buttons to Toolbar
    Also add notification toasts when user signs in/out or fails to do it

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <v-app>
-    <Toolbar @drawer-toggle="callToggleDrawer"/>
+    <Toolbar
+      :user="user"
+      @drawer-toggle="callToggleDrawer"
+      @sign-in="handleSignIn"
+      @sign-out="handleSignOut"
+    />
     <NavigationDrawer ref="drawer" :menuItems="drawerMenuItems"/>
     <v-content>
       <v-container fluid fill-height>
         <router-view/>
       </v-container>
     </v-content>
+
+    <v-snackbar v-model="snackbar">
+      {{snackBarText}}
+      <v-btn flat color="pink" @click.native="snackbar = false">Close</v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
+import { mapState } from "vuex";
 import NavigationDrawer from "@/components/NavigationDrawer.vue";
 import Toolbar from "@/components/Toolbar.vue";
+import { signIn, signOut } from "@/firebase/auth";
 
 export default {
   name: "Hive",
@@ -22,15 +34,43 @@ export default {
   },
   data() {
     return {
+      snackbar: false,
+      snackBarText: "",
+      snackBarTimeOut: 2000,
       drawerMenuItems: [
         { text: "Home", icon: "home", action: "/" },
         { text: "About", icon: "contact_mail", action: "/about" }
       ]
     };
   },
+  computed: {
+    ...mapState(["user"])
+  },
   methods: {
     callToggleDrawer() {
       this.$refs["drawer"].toggleDrawer();
+    },
+    handleSignIn() {
+      signIn()
+        .then(() => {
+          this.showSnackBar("Signed in!");
+        })
+        .catch(() => {
+          this.showSnackBar("Failed to sign in");
+        });
+    },
+    handleSignOut() {
+      signOut()
+        .then(() => {
+          this.showSnackBar("Signed out!");
+        })
+        .catch(() => {
+          this.showSnackBar("Failed to sign out");
+        });
+    },
+    showSnackBar(text) {
+      this.snackBarText = text;
+      this.snackbar = true;
     }
   }
 };
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -2,5 +2,31 @@
   <v-toolbar color="indigo" dark fixed app>
     <v-toolbar-side-icon @click.stop="$emit('drawer-toggle')"></v-toolbar-side-icon>
     <v-toolbar-title>Hive</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn
+        v-if="!user"
+        @click="$emit('sign-in')"
+        flat
+      >Sign In</v-btn>
+      <v-menu v-else offset-y>
+        <v-btn
+          flat
+          slot="activator"
+        >{{user.displayName}}</v-btn>
+        <v-list>
+          <v-list-tile @click.stop="$emit('sign-out')">
+            <v-list-tile-title>Sign Out</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
+
+<script>
+export default {
+  name: "Toolbar",
+  props: ["user"]
+}
+</script>
