4c1f6417f006e06987514c09ffcc348052ef6d9d
João Fernandes <joaovitorfernandes2@gmail.com>
2018-06-08 05:08:55 -0300

    Remove validations from AgendaForm

--- src/components/AgendaForm.vue
+++ src/components/AgendaForm.vue
@@ -1,19 +1,17 @@
 <template>
-  <v-form ref="form" v-model="isValid">
+  <v-form ref="agendaForm">
     <v-text-field
       v-model="title"
       label="Title"
-      :rules="titleRules"
       required
     ></v-text-field>
     <v-text-field
       v-model="content"
       label="Content"
-      :rules="contentRules"
       multi-line
       required
     ></v-text-field>
-    <v-btn :disabled="!isValid" @click="submit">add agenda</v-btn>
+    <v-btn @click="submit">add agenda</v-btn>
   </v-form>
 </template>
 
@@ -23,20 +21,17 @@ export default {
   data: () => ({
     isValid: false,
     title: "",
-    content: "",
-    titleRules: [v => !!v],
-    contentRules: [v => !!v]
+    content: ""
   }),
   methods: {
     submit() {
-      if (this.$refs.form.validate()) {
-        this.$emit("submit", {
-          title: this.title,
-          content: this.content
-        });
+      this.$emit("submit", {
+        title: this.title,
+        content: this.content,
+        questionsNum: 0
+      });
 
-        this.$refs.form.reset();
-      }
+      this.$refs.agendaForm.reset();
     }
   }
 };
