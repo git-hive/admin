f271f285d2e389be6cc9a9ce6af56eb3f41206f3
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2018-06-18 09:26:16 -0300

    Add file view into association items

---
+++ src/views/association/Files.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container elevation-4 class="pa-0">
+    <v-dialog>
+      <v-tooltip slot="activator" left>
+        <v-btn slot="activator" color="primary" fixed fab bottom right>
+          <v-icon>add</v-icon>
+        </v-btn>
+        <span>Upload file</span>
+      </v-tooltip>
+
+    <file-form @submit="handleFileUpload"/>
+
+    </v-dialog>
+
+    <v-tabs fixed-tabs>
+      <v-tab>Files</v-tab>
+      <v-tab-item>
+        <v-list>
+          <v-list-tile v-for="file in files" :key="file.id" @click="() => {}">
+            <v-list-tile-content>
+              <v-list-tile-title>{{file.get("name")}}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-tab-item>
+    </v-tabs>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import {
+  getAllAssociationFileSnaps,
+  uploadAssociationFile
+} from "@/firebase/firestore/associations/files";
+
+import FileForm from "@/components/FileForm.vue";
+
+export default {
+  name: "files",
+  components: { FileForm },
+  data: () => ({
+    files: []
+  }),
+  computed: {
+    ...mapState(["selectedAssociation"])
+  },
+  mounted() {
+    this.fetchFilessAndSet();
+  },
+  methods: {
+    handleFileUpload({ file, file_name }) {
+      uploadAssociationFile(file, file_name, this.selectedAssociation.id);
+    },
+    async fetchFilessAndSet() {
+      this.files = await getAllAssociationFileSnaps(this.selectedAssociation.id);
+    }
+  }
+};
+</script>
