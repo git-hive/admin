a1c2e177df6aaac4c74656871d5189c6cc11fd88
João Fernandes <joaovitorfernandes2@gmail.com>
2018-06-24 03:08:01 -0300

    Display member requests data

--- src/views/MemberRequests.vue
+++ src/views/MemberRequests.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>Member Requests</h1>
+  <v-container elevation-4 class="pa-0">
+    <v-list>
+      <v-expansion-panel>
+        <v-expansion-panel-content
+          v-for="request in requests"
+          :key="request.id"
+          @click="() => {}"
+        >
+          <div slot="header">
+            <v-list-tile>
+              <v-list-tile-avatar>
+                <img :src="request.get('photoUrl')">
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{request.get("name")}}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </div>
+
+          <member-request-tile :member-request="request"></member-request-tile>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-list>
+  </v-container>
 </template>
+
+<script>
+import { mapState } from "vuex";
+import { getAllMemberRequestsDocs } from "@/firebase/firestore/associations/memberRequests";
+import MemberRequestTile from "@/components/MemberRequestTile.vue"
+export default {
+  name: "member-requests",
+  components: {
+    MemberRequestTile
+  },
+  data: () => ({
+    requests: []
+  }),
+  computed: {
+    ...mapState(["selectedAssociation"]),
+  },
+  async mounted() {
+    this.requests = await getAllMemberRequestsDocs(this.selectedAssociation.id)
+  }
+}
+</script>
