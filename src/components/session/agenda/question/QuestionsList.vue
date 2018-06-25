0c9c75356f277ab3f294ea7fb0dd2f1d6089def8
João Fernandes <joaovitorfernandes2@gmail.com>
2018-06-25 10:44:07 -0300

    Create QuestionsList component

---
+++ src/components/session/agenda/question/QuestionsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-flex>
+    <h1 class="subheading ml-2">Tópicos</h1>
+    <v-expansion-panel v-for="(question, i) in questions" :key="i">
+      <v-expansion-panel-content>
+        <v-list slot="header">
+          <h3>{{question.question}}</h3>
+          <span>{{question.info}}</span>
+        </v-list>
+        <v-card>
+        <options-list :options="question.options"></options-list>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-flex>
+</template>
+
+<script>
+import OptionsList from "./OptionsList.vue";
+
+export default {
+  name: "questions-list",
+  props: ["questions"],
+  components: {
+    OptionsList
+  }
+};
+</script>
