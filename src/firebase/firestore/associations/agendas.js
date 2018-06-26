a5be69ed999405ddf6e34faeae3b842a0429cbf2
João Fernandes <joaovitorfernandes2@gmail.com>
2018-06-25 21:58:03 -0300

    Add agenda reference getter

--- src/firebase/firestore/associations/agendas.js
+++ src/firebase/firestore/associations/agendas.js
@@ -6,6 +6,16 @@ const db = firestore();
 db.settings({ timestampsInSnapshots: true });
 
 /**
+ * Gets the reference to agendas collection
+ *
+ * @param {firestore.DocumentReference} sessionRef Self descriptive
+ * @returns {firestore.CollectionReference}
+ */
+export function agendasRef(sessionRef) {
+  return sessionRef.collection(AGENDAS_COLLECTION);
+}
+
+/**
  * Add agendas to the session
  *
  * @param {firestore.DocumentReference} sessionRef Session document where to add
