d2ce790105f719249e995927d0d982ada8c2faed
João Fernandes <joaovitorfernandes2@gmail.com>
2018-07-19 08:17:46 -0300

    Refactor QuestionsList to use a list group

--- src/components/session/agenda/question/QuestionsList.vue
+++ src/components/session/agenda/question/QuestionsList.vue
@@ -1,27 +1,26 @@
 <template>
   <v-flex>
     <h1 v-if="questions.length !== 0" class="subheading ml-2">Tópicos</h1>
-    <v-expansion-panel v-for="(question, i) in questions" :key="i">
-      <v-expansion-panel-content>
-        <v-list slot="header">
-          <v-list-tile>
-            <v-list-tile-content>
+
+    <v-list v-for="(question, i) in questions" :key="i">
+      <v-list-group>
+        <v-list-tile slot="activator">
+          <v-list-tile-content>
               <v-list-tile-title>{{question.question}}</v-list-tile-title>
               <v-list-tile-sub-title>{{question.info}}</v-list-tile-sub-title>
-            </v-list-tile-content>
-            <v-list-tile-action
-              v-if="showAction"
-              @click="$emit('actionClick', i)"
-            >
-              <v-icon>{{actionIcon}}</v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-        <v-card>
+          </v-list-tile-content>
+
+          <v-list-tile-action
+            v-if="showAction"
+            @click="$emit('actionClick', i)"
+          >
+            <v-icon>{{actionIcon}}</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+
         <options-list :options="question.options"></options-list>
-        </v-card>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+      </v-list-group>
+    </v-list>
   </v-flex>
 </template>
 
