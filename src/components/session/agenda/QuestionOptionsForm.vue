938418bb41ad5fc31b93847845f53a80dc843482
João Fernandes <joaovitorfernandes2@gmail.com>
2018-07-19 08:16:46 -0300

    Use VeeValidate to validate QuestionOptionsForm

--- src/components/session/agenda/QuestionOptionsForm.vue
+++ src/components/session/agenda/QuestionOptionsForm.vue
@@ -3,14 +3,20 @@
     <v-text-field
       v-model="title"
       label="Título"
+      v-validate="'required'"
+      data-vv-name="title"
+      :error-messages="errors.collect('title')"
       required
     ></v-text-field>
     <v-textarea
       v-model="content"
       label="Subtítulo"
+      v-validate="'required'"
+      data-vv-name="subTitle"
+      :error-messages="errors.collect('subTitle')"
       required
     ></v-textarea>
-    <v-btn @click="submit">Adicionar opção</v-btn>
+    <v-btn color="primary" @click="submit">Criar opção</v-btn>
   </v-form>
 </template>
 
@@ -23,13 +29,17 @@ export default {
     content: ""
   }),
   methods: {
-    submit() {
+    async submit() {
+      const formIsValid = await this.$validator.validateAll();
+      if (!formIsValid) return;
+
       this.$emit("submit", {
         title: this.title,
         content: this.content
       });
 
       this.$refs.questionOptionForm.reset();
+      this.$validator.reset();
     }
   }
 };
