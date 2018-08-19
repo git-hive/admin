1820e19a8213fdadbf97499381a1eb1fd3308e55
João Fernandes <joaovitorfernandes2@gmail.com>
2018-08-19 16:52:18 -0300

    Add PieChart Component

---
+++ src/components/charts/PieChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js";
+import colors from "@/config/colors";
+
+export default {
+  name: "pie-chart",
+  props: ["data", "options"],
+  data() {
+    return {
+      chart: null
+    };
+  },
+  computed: {
+    colors() {
+      return Object.values(colors);
+    }
+  },
+  mounted() {
+    const datasetWithColors = this.data.datasets;
+    datasetWithColors.forEach(ds => {
+      ds.backgroundColor = Object.values(colors);
+    });
+    this.data.dataset = datasetWithColors;
+    const ctx = this.$refs["canvas"].getContext("2d");
+    this.chart = new Chart(ctx, {
+      type: "pie",
+      data: this.data,
+      options: Object.assign({ colors: this.colors }, this.options)
+    });
+  }
+};
+</script>
