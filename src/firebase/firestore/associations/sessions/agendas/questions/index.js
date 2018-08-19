10f0908ad7f0647b0dbb7c8416f67921f355eca3
João Fernandes <joaovitorfernandes2@gmail.com>
2018-08-19 00:31:31 -0300

    Removed options helper since they were bundled with questions docs

--- src/firebase/firestore/associations/sessions/agendas/questions/index.js
+++ src/firebase/firestore/associations/sessions/agendas/questions/index.js
@@ -1,7 +1,6 @@
 import { firestore } from "firebase";
 
 const QUESTIONS_COLLECTION = "questions";
-const OPTIONS_COLLECTION = "options";
 
 const db = firestore();
 db.settings({ timestampsInSnapshots: true });
@@ -14,13 +13,4 @@ db.settings({ timestampsInSnapshots: true });
  */
 export function questionsRef(agendaRef) {
   return agendaRef.collection(QUESTIONS_COLLECTION);
-}
-
-/**
- *
- * @param {firestore.DocumentReference} questionRef Self descriptive
- * @returns {firestore.CollectionReference}
- */
-export function optionsRef(questionRef) {
-  return questionRef.collection(OPTIONS_COLLECTION);
 }
