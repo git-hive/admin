47b9e838cd5fefc8292f92ebeb4345088d89c675
João Fernandes <joaovitorfernandes2@gmail.com>
2018-08-19 16:53:58 -0300

    Display votes with PieChart

--- src/components/session/agenda/question/QuestionsList.vue
+++ src/components/session/agenda/question/QuestionsList.vue
@@ -6,8 +6,8 @@
       <v-list-group>
         <v-list-tile slot="activator">
           <v-list-tile-content>
-              <v-list-tile-title>{{question.question}}</v-list-tile-title>
-              <v-list-tile-sub-title>{{question.info}}</v-list-tile-sub-title>
+            <v-list-tile-title>{{question.question}}</v-list-tile-title>
+            <v-list-tile-sub-title>{{question.info}}</v-list-tile-sub-title>
           </v-list-tile-content>
 
           <v-list-tile-action
@@ -18,6 +18,8 @@
           </v-list-tile-action>
         </v-list-tile>
 
+        <pie-chart :data="chartDataAdapter(question.options)"></pie-chart>
+
         <options-list :options="question.options"></options-list>
       </v-list-group>
     </v-list>
@@ -26,16 +28,31 @@
 
 <script>
 import OptionsList from "./OptionsList.vue";
+import PieChart from "@/components/charts/PieChart.vue";
 
 export default {
   name: "questions-list",
   props: ["questions", "showAction", "actionIcon"],
   components: {
-    OptionsList
+    OptionsList,
+    PieChart
   },
   computed: {
     hasQuestions() {
       return !!(this.questions && this.questions.length);
+    }
+  },
+  methods: {
+    chartDataAdapter: function(options) {
+      return {
+        labels: options.map(o => o.title),
+        datasets: [
+          {
+            label: "# de votos",
+            data: options.map(o => o.score)
+          }
+        ]
+      };
     }
   }
 };
